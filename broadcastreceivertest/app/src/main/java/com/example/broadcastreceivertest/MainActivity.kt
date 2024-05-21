4c627c1450819631bfd77fcd1511974454bfd673
Junyoung Heo <jyheo0@gmail.com>
2024-05-21 22:37:38 +0900

    broadcast receiver test

---
+++ broadcastreceivertest/app/src/main/java/com/example/broadcastreceivertest/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.broadcastreceivertest
+
+import android.Manifest
+import android.content.*
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private val broadcastReceiver = MyBroadcastReceiver()
+    private val buttonSendMyBroad: Button by lazy { findViewById(R.id.buttonSendMyBroad) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestSinglePermission(Manifest.permission.RECEIVE_SMS)
+
+        buttonSendMyBroad.setOnClickListener {
+            sendBroadcast(Intent(ACTION_MY_BROADCAST))
+        }
+    }
+
+    private fun requestSinglePermission(permission: String) {
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED)
+            return
+
+        val requestPermLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it == false) { // permission is not granted!
+                AlertDialog.Builder(this).apply {
+                    setTitle("Warning")
+                    setMessage(getString(R.string.no_permission, permission))
+                }.show()
+            }
+        }
+
+        if (shouldShowRequestPermissionRationale(permission)) {
+            // you should explain the reason why this app needs the permission.
+            AlertDialog.Builder(this).apply {
+                setTitle("Reason")
+                setMessage(getString(R.string.req_permission_reason, permission))
+                setPositiveButton("Allow") { _, _ -> requestPermLauncher.launch(permission) }
+                setNegativeButton("Deny") { _, _ -> }
+            }.show()
+        } else {
+            // should be called in onCreate()
+            requestPermLauncher.launch(permission)
+        }
+    }
+
+    private fun startBroadcastReceiver() {
+        IntentFilter().also {
+            it.addAction(Intent.ACTION_POWER_CONNECTED)
+            it.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            it.addAction("android.provider.Telephony.SMS_RECEIVED")
+            it.addAction(ACTION_MY_BROADCAST)
+            registerReceiver(broadcastReceiver, it)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startBroadcastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
+    }
+
+    inner class MyBroadcastReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                Intent.ACTION_POWER_CONNECTED -> { showBroadcast(Intent.ACTION_POWER_CONNECTED) }
+                ACTION_MY_BROADCAST -> { showBroadcast(ACTION_MY_BROADCAST) }
+                else -> { showBroadcast(intent?.action ?: "NO ACTION")}
+            }
+        }
+
+        private fun showBroadcast(msg: String) {
+            println(msg)
+            Snackbar.make(buttonSendMyBroad, msg, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        const val ACTION_MY_BROADCAST = "ACTION_MY_BROADCAST"
+    }
+}
---
+++ broadcastreceivertest/readme.txt
@@ -0,0 +1,3 @@
+make a new project in this folder.
+The package name must be "com.example.broadcastreceivertest"
+
